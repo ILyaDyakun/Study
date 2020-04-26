290f2b9b093cfd7414cde297ff152516f70ebf03
ILya Dyakun <aamelgal@gmail.com>
2020-04-26 17:21:41 +0300

    HW6 fixed1

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,9 +1,9 @@
-let check = +prompt('input check number', 0);
-let tip = +prompt('input tip percentage', 0);
+let check = prompt('input check number', 0);
+let tip = prompt('input tip percentage', 0);
 const hundred = 100;
 const minPer = 0;
 const maxPer = 100;
-if (!isNaN(check) && !isNaN(tip) && check >= 0 && tip >= minPer && tip <= maxPer) {
+if (!isNaN(parseFloat(check)) && isFinite(check) && !isNaN(parseFloat(tip)) && isFinite(tip) && check >= 0 && tip >= minPer && tip <= maxPer) {
     let tipAmount = tip * check / hundred;
     let sum = check + tipAmount;
     alert(`Check number: ${check}
