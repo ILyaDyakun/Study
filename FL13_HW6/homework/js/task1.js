50160295741be4c6d153cb57d12fadacd4767e35
ILya Dyakun <aamelgal@gmail.com>
2020-04-26 21:55:48 +0300

    HW6 fixed3

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -3,13 +3,14 @@ let tip = prompt('input tip percentage', 0);
 const hundred = 100;
 const minPer = 0;
 const maxPer = 100;
-if (!isNaN(parseFloat(check)) && isFinite(check) && !isNaN(parseFloat(tip)) && isFinite(tip) && check >= 0 && tip >= minPer && tip <= maxPer) {
+if (!isNaN(parseFloat(check)) && isFinite(check) && !isNaN(parseFloat(tip)) && isFinite(tip) 
+&& check >= 0 && tip >= minPer && tip <= maxPer) {
     let tipAmount = tip * check / hundred;
     let sum = +check + tipAmount;
-    alert(`Check number: ${Math.trunc(check * 100) / 100}
-Tip: ${Math.trunc(tip * 100) / 100}%
-Tip amount: ${Math.trunc(tipAmount * 100) / 100}
-Total sum to pay: ${Math.trunc(sum * 100) / 100}`);
+    alert(`Check number: ${Math.trunc(check * hundred) / hundred}
+Tip: ${Math.trunc(tip * hundred) / hundred}%
+Tip amount: ${Math.trunc(tipAmount * hundred) / hundred}
+Total sum to pay: ${Math.trunc(sum * hundred) / hundred}`);
 } else {
     alert('Invalid input data');
 }
